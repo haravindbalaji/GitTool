1fb28123baaf2acd85ce3b0c1eebe24e96c887ce
haravindbalaji <h.aravindbalaji@gmail.com>
2023-04-19 15:27:34 +0530

    project commiting

---
+++ src/org/test/gittool/GitTool.java
@@ -0,0 +1,13 @@
+package org.test.gittool;
+
+public class GitTool {
+	public void test() {
+		System.out.println("test");
+	}
+	
+	public static void main(String[] args) {
+		GitTool g = new GitTool();
+		g.test();
+				
+	}
+}
